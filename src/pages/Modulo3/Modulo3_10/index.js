9f9d735ccf3eb661ad8746a34216add789b01ce7
Viviane Ferreira <vferreiradeoliveira595@gmail.com>
2023-10-21 10:51:39 -0300

    Ajustes do modulo 3

--- src/pages/Modulo3/Modulo3_10/index.js
+++ src/pages/Modulo3/Modulo3_10/index.js
@@ -3,150 +3,149 @@ import Layout from '../../../components/Layout';
 
 import Pagination from '../../../components/Pagination';
 
-
 import './styles.css';
 import { Link } from 'react-router-dom';
 
 function Modulo3_10() {
-
-
 	return (
 		<Layout>
-			<div className='container'>
-                <div className='contentCardModulo3_10'>
-                    <div className='aling-line-Modulo3_10'>
-                        <div className='cardInfoApo1'>
-                            <p>Framework de gerenciamento de TI gerenciado</p>
-                        </div>
-                        <div className='line-cardInfoApo1'></div>
-                    </div>
-                    <div className='aling-line-Modulo3_10'>
-                        <div className='cardInfoApo1'>
-                            <p>Estrategia gerenciada</p>
-                        </div>
-                        <div className='line-cardInfoApo1'></div>
-                    </div>
-                    <div className='aling-line-Modulo3_10'>
-                        <div className='cardInfoApo1'>
-                            <p>Arquitetira empresarial gerenciada</p>
-                        </div>
-                        <div className='line-cardInfoApo1'></div>
-                    </div>
-                    <div className='aling-line-Modulo3_10'>
-                        <div className='cardInfoApo1'>
-                            <p>Inovação gerenciada</p>
-                        </div>
-                        <div className='line-cardInfoApo1'></div>
-                    </div>
-                    <div className='aling-line-Modulo3_10'>
-                        <div className='cardInfoApo1'>
-                            <p>Portifolio gerenciado</p>
-                        </div>
-                        <div className='line-cardInfoApo1'></div>
-                    </div>
-                    <div className='aling-line-Modulo3_10'>
-                        <div className='cardInfoApo1'>
-                            <p>Orçamento e custos gerenciados</p>
-                        </div>
-                        <div className='line-cardInfoApo1'></div>
-                    </div>
-                    <div className='aling-line-Modulo3_10'>
-                        <div className='cardInfoApo1'>
-                            <p>Recursos humanos gerenciados</p>
-                        </div>
-                        <div className='line-cardInfoApo1'></div>
-                    </div>
-                </div>
-                <div className='contentModulo3_10'>
-                    <div className='apoBackgroundModulo3_10'>
-                        <div className='cardModulo3_10'>
-                            <p className='textApoModulo3_10'>APO 1</p>
-                        </div>
-                        <div className='cardModulo3_10'>
-                            <p className='textApoModulo3_10'>APO 2</p>
-                        </div>
-                        <div className='cardModulo3_10'>
-                            <p className='textApoModulo3_10'>APO 3</p>
-                        </div>
-                        <div className='cardModulo3_10'>
-                            <p className='textApoModulo3_10'>APO 4</p>
-                        </div>
-                        <div className='cardModulo3_10'>
-                            <p className='textApoModulo3_10'>APO 5</p>
-                        </div>
-                        <div className='cardModulo3_10'>
-                            <p className='textApoModulo3_10'>APO 6</p>
-                        </div>
-                        <div className='cardModulo3_10'>
-                            <p className='textApoModulo3_10'>APO 7</p>
-                        </div>
-                        <div className='cardModulo3_10'>
-                            <p className='textApoModulo3_10'>APO 8</p>
-                        </div>
-                        <div className='cardModulo3_10'>
-                            <p className='textApoModulo3_10'>APO 9</p>
-                        </div>
-                        <div className='cardModulo3_10'>
-                            <p className='textApoModulo3_10'>APO 10</p>
-                        </div>
-                        <div className='cardModulo3_10'>
-                            <p className='textApoModulo3_10'>APO 11</p>
-                        </div>
-                        <div className='cardModulo3_10'>
-                            <p className='textApoModulo3_10'>APO 12</p>
-                        </div>
-                        <div className='cardModulo3_10'>
-                            <p className='textApoModulo3_10'>APO 13</p>
-                        </div>
-                        <div className='cardModulo3_10'>
-                            <p className='textApoModulo3_10'>APO 14</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='contentCardModulo3_10'>
-                    <div className='aling-line-Modulo3_10'>
-                        <div className='line-cardInfoApo1'></div>
-                        <div className='cardInfoApo1'>
-                            <p>Relacionamentos gerenciados</p>
-                        </div>
-                    </div>
-                    <div className='aling-line-Modulo3_10'>
-                        <div className='line-cardInfoApo1'></div>
-                        <div className='cardInfoApo1'>
-                            <p>Acordos de serviço gerenciados</p>
-                        </div>
-                    </div>
-                    <div className='aling-line-Modulo3_10'>
-                        <div className='line-cardInfoApo1'></div>
-                        <div className='cardInfoApo1'>
-                            <p>Terceiros Gerenciados</p>
-                        </div>
-                    </div>
-                    <div className='aling-line-Modulo3_10'>
-                        <div className='line-cardInfoApo1'></div>
-                        <div className='cardInfoApo1'>
-                            <p>Qualidade gerenciada</p>
-                        </div>
-                    </div>
-                    <div className='aling-line-Modulo3_10'>
-                        <div className='line-cardInfoApo1'></div>
-                        <div className='cardInfoApo1'>
-                            <p>Risco gerenciado</p>
-                        </div>
-                    </div>
-                    <div className='aling-line-Modulo3_10'>
-                        <div className='line-cardInfoApo1'></div>
-                        <div className='cardInfoApo1'>
-                            <p>Segurança gerenciada</p>
-                        </div>
-                    </div>
-                    <div className='aling-line-Modulo3_10'>
-                        <div className='line-cardInfoApo1'></div>
-                        <div className='cardInfoApo1'>
-                            <p>Framework de gerenciamento de TI gerenciado</p>
-                        </div>
-                    </div>
-                </div>
+			<div className='containerModulo3_10'>
+				<div className='contentCardModulo3_10'>
+					<div className='aling-line-Modulo3_10'>
+						<div className='cardInfoApo1'>
+							<p>Framework de gerenciamento de TI gerenciado</p>
+						</div>
+						<div className='line-cardInfoApo1'></div>
+					</div>
+					<div className='aling-line-Modulo3_10'>
+						<div className='cardInfoApo1'>
+							<p>Estrategia gerenciada</p>
+						</div>
+						<div className='line-cardInfoApo1'></div>
+					</div>
+					<div className='aling-line-Modulo3_10'>
+						<div className='cardInfoApo1'>
+							<p>Arquitetira empresarial gerenciada</p>
+						</div>
+						<div className='line-cardInfoApo1'></div>
+					</div>
+					<div className='aling-line-Modulo3_10'>
+						<div className='cardInfoApo1'>
+							<p>Inovação gerenciada</p>
+						</div>
+						<div className='line-cardInfoApo1'></div>
+					</div>
+					<div className='aling-line-Modulo3_10'>
+						<div className='cardInfoApo1'>
+							<p>Portifolio gerenciado</p>
+						</div>
+						<div className='line-cardInfoApo1'></div>
+					</div>
+					<div className='aling-line-Modulo3_10'>
+						<div className='cardInfoApo1'>
+							<p>Orçamento e custos gerenciados</p>
+						</div>
+						<div className='line-cardInfoApo1'></div>
+					</div>
+					<div className='aling-line-Modulo3_10'>
+						<div className='cardInfoApo1'>
+							<p>Recursos humanos gerenciados</p>
+						</div>
+						<div className='line-cardInfoApo1'></div>
+					</div>
+				</div>
+				<div className='contentModulo3_10'>
+					<div className='cardApoBaiDssMea'>
+						<Link to='/Modulo3_10' className='apoBackground'>
+							<div className='cardModulo3_8'>
+								<p className='textEdmModulo3_8'>APO 1</p>
+							</div>
+							<div className='cardModulo3_8'>
+								<p className='textEdmModulo3_8'>APO 2</p>
+							</div>
+							<div className='cardModulo3_8'>
+								<p className='textEdmModulo3_8'>APO 3</p>
+							</div>
+							<div className='cardModulo3_8'>
+								<p className='textEdmModulo3_8'>APO 4</p>
+							</div>
+							<div className='cardModulo3_8'>
+								<p className='textEdmModulo3_8'>APO 5</p>
+							</div>
+							<div className='cardModulo3_8'>
+								<p className='textEdmModulo3_8'>APO 6</p>
+							</div>
+							<div className='cardModulo3_8'>
+								<p className='textEdmModulo3_8'>APO 7</p>
+							</div>
+							<div className='cardModulo3_8'>
+								<p className='textEdmModulo3_8'>APO 8</p>
+							</div>
+							<div className='cardModulo3_8'>
+								<p className='textEdmModulo3_8'>APO 9</p>
+							</div>
+							<div className='cardModulo3_8'>
+								<p className='textEdmModulo3_8'>APO 10</p>
+							</div>
+							<div className='cardModulo3_8'>
+								<p className='textEdmModulo3_8'>APO 11</p>
+							</div>
+							<div className='cardModulo3_8'>
+								<p className='textEdmModulo3_8'>APO 12</p>
+							</div>
+							<div className='cardModulo3_8'>
+								<p className='textEdmModulo3_8'>APO 13</p>
+							</div>
+							<div className='cardModulo3_8'>
+								<p className='textEdmModulo3_8'>APO 14</p>
+							</div>
+						</Link>
+					</div>
+				</div>
+				<div className='contentCardModulo3_10'>
+					<div className='aling-line-Modulo3_10'>
+						<div className='line-cardInfoApo1'></div>
+						<div className='cardInfoApo1'>
+							<p>Relacionamentos gerenciados</p>
+						</div>
+					</div>
+					<div className='aling-line-Modulo3_10'>
+						<div className='line-cardInfoApo1'></div>
+						<div className='cardInfoApo1'>
+							<p>Acordos de serviço gerenciados</p>
+						</div>
+					</div>
+					<div className='aling-line-Modulo3_10'>
+						<div className='line-cardInfoApo1'></div>
+						<div className='cardInfoApo1'>
+							<p>Terceiros Gerenciados</p>
+						</div>
+					</div>
+					<div className='aling-line-Modulo3_10'>
+						<div className='line-cardInfoApo1'></div>
+						<div className='cardInfoApo1'>
+							<p>Qualidade gerenciada</p>
+						</div>
+					</div>
+					<div className='aling-line-Modulo3_10'>
+						<div className='line-cardInfoApo1'></div>
+						<div className='cardInfoApo1'>
+							<p>Risco gerenciado</p>
+						</div>
+					</div>
+					<div className='aling-line-Modulo3_10'>
+						<div className='line-cardInfoApo1'></div>
+						<div className='cardInfoApo1'>
+							<p>Segurança gerenciada</p>
+						</div>
+					</div>
+					<div className='aling-line-Modulo3_10'>
+						<div className='line-cardInfoApo1'></div>
+						<div className='cardInfoApo1'>
+							<p>Framework de gerenciamento de TI gerenciado</p>
+						</div>
+					</div>
+				</div>
 			</div>
 			<Pagination up='/Modulo3_8' down='/Modulo3_8' />
 		</Layout>
