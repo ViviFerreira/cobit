2e3da94cdd245cb5848d0c6022a33977361b0dc8
nalberthdionisio <nalberthdionisio@aluno.fapce.edu.br>
2023-11-11 20:40:30 -0300

    json-server

--- src/providers/points.js
+++ src/providers/points.js
@@ -32,7 +32,7 @@ export const PointsProvider = (props) => {
 		async function fetchData() {
 			try {
 				if (idUsuario > 0) {
-					const data = await buscar(`http://localhost:3001/points/${idUsuario}/`);
+					const data = await buscar(`https://json-server-gamma-nine.vercel.app/points/${idUsuario}/`);
 					setDados(data);
 					setModulosDesbloqueados(data.unlockedModules);
 					setCompleteQuestions(data.completeQuestions);
