2209c9a7d187e9200e7eb984d1dbd83715c24a2c
nalberthdionisio <73546625+nalberthdionisio@users.noreply.github.com>
2023-11-12 17:35:37 -0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-	baseURL: 'http://127.0.0.1:3001',
+	baseURL: 'https://server-js-theta.vercel.app',
 });
 
 export const cadastrar = async (values) => {
